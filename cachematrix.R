4fc5115bc199b6570b9e3a78acb2400009b77974
leclearnm <leclearnm@utexas.edu>
2020-05-19 13:32:26 -0500

    Edited R file for second programming assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions work together. makeCacheMatrix takes a matrix
+## as input, creates an empty variable to hold the inverse, and 
+## outputs a list of functions for use by cacheSolve. cacheSolve
+## retrieves the inverse object, checks and returns the value
+## if it holds an inverse, otherwise retrieve the initial matrix 
+## input and calculates the inverse, storing the result in the 
+## original environment.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object 
+## that can cache its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(M = matrix()) {
+      Mi <- NULL #empty object to hold the inverse matrix
+      setM <- function(newMatrix){ #setter that takes new user input
+            M <<- newMatrix       #after object mcm is created
+            Mi <<- NULL
+      }
+      getM <- function() M #Returns the user input matrix
+      setMi <- function(inv) Mi <<- inv
+      getMi <- function() Mi
+      list(setM = setM, getM = getM, 
+           setMi = setMi, getMi = getMi)
 }
 
 
-## Write a short comment describing this function
+## This function takes object "mcm" as an argument, collects the
+## inverse object from above, checks if it is already cached,
+## returns it if it is, otherwise retrieves matrix M and
+## calculates, caches, and returns its inverse
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mcm) {
+      Mi <- mcm$getMi() #retrieve object Mi from mcm environment
+      if(!is.null(Mi)) {  #check if inverse exists already
+            message("retrieving cached inverse")
+            return(Mi)
+      }
+      mat <- mcm$getM()
+      Mi <- solve(mat)
+      mcm$setMi(Mi) #set the new inverse to Mi in mcm environment
+      Mi ## Return a matrix that is the inverse of 'M'
 }
